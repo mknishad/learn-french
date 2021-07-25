0b202c8f5be4a21b077e78e12bc5953381bcab03
mknishad <mknishad1@ymail.com>
2021-07-26 00:52:37 +0600

    migrate to androidx

--- app/src/main/java/com/ideafactorybd/learnfrench/CategoryAdapter.java
+++ app/src/main/java/com/ideafactorybd/learnfrench/CategoryAdapter.java
@@ -1,9 +1,9 @@
 package com.ideafactorybd.learnfrench;
 
 import android.content.Context;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 
 /**
  * Created by Nishad on 5/8/2017.
